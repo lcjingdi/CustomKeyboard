3abb08db9adb0d09c23c57e48533cab910c0bcfa
lcjingdi <lcjingdi@gmail.com>
2017-06-21 11:30:02 +0800

    Initial Commit

---
+++ customKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customKeyboard
+//
+//  Created by jingdi on 2017/6/21.
+//  Copyright © 2017年 ekwing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
